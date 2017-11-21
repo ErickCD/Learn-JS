76683464e7ef609468ac8f5cfe906fa47ffca495
Clair <acheron_1405@hotmail.com>
2017-11-21 12:44:51 -0600

    Capitulo 2 del libro - variables, funciones y hola mundo desde un servidor local, hecho con Nodejs

---
+++ Chapter2/1_variables.js
@@ -0,0 +1,23 @@
+// Chapter2
+var ten = 10;
+console.log(ten * ten);
+//-> 100
+
+//variables de tipo cadena
+console.log("");
+var mood = "light";
+console.log("mood = " + mood);
+mood = "dark";
+console.log("mood = " + mood);
+
+//Cambios de dinero
+console.log("");
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log("luigisDebt = " + luigisDebt);
+// -> 105
+
+console.log("");
+var one = 1, two = 2;
+console.log("(variables) one + tow = " + (one + two));
+
---
+++ Chapter2/2_functions.js
@@ -0,0 +1,4 @@
+// Chapter 2 - functions
+//No funcionan sin el entorno web
+//alert("Good morning");
+//confirm("Shall we, then");
