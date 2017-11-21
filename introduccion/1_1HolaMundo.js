a12791d93b913b250e18cd0cbc6bf8a810af8087
Clair <acheron_1405@hotmail.com>
2017-11-21 12:46:21 -0600

    Conceptos basicos desde consola y Hola mundo

---
+++ introduccion/1_1HolaMundo.js
@@ -0,0 +1,2 @@
+console.log("¡Hola mundo!");
+console.log("Hello world!");
---
+++ introduccion/1_primerjs2.js
@@ -0,0 +1,11 @@
+//var total = 0, count = 1;
+/*
+while (count <= 10) {
+    total += count;
+    count += 1;
+}
+
+console.log(total);
+// -> 55
+*/
+WScript.Echo("Simple JScript File");
